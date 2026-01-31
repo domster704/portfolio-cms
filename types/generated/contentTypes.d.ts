53ae485d0d3242da72adf2be4735c41960578d90
domster704 <domster704@mail.ru>
2026-01-31 22:36:41 +0300

    add order field to Job

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -397,6 +397,7 @@ export interface ApiJobJob extends Struct.CollectionTypeSchema {
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::job.job'> &
       Schema.Attribute.Private;
+    order: Schema.Attribute.Integer;
     position: Schema.Attribute.String;
     publishedAt: Schema.Attribute.DateTime;
     skills: Schema.Attribute.JSON;
